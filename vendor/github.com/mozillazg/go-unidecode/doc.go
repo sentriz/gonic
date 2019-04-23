f2a2a70dc401170e1b30bfe0be5113903d6c9dcb
sentriz <senan@senan.xyz>
2019-04-23 15:17:07 +0100

    g

--- vendor/github.com/mozillazg/go-unidecode/doc.go
+++
@@ -1,6 +0,0 @@
-// Package unidecode provide ASCII transliterations of Unicode text
-//
-// 	s := "北京kožušček"
-// 	fmt.Println(unidecode.Unidecode(s))
-// 	// Output: Bei Jing kozuscek
-package unidecode
