de807902de9e2eadd467236621676b61b9893157
sentriz <senan@senan.xyz>
2019-04-30 17:16:47 +0100

    delete handler test

--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"log"
-	"path/filepath"
 	"runtime"
 
 	"github.com/jinzhu/gorm"
@@ -18,17 +17,6 @@ func New() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "gonic.db")
 	if err != nil {
 		log.Printf("when opening database: %v\n", err)
-	}
-	return db
-}
-
-// New creates a new GORM connection to the mock database
-func NewMock() *gorm.DB {
-	// projectRoot presumes this file is `<root>/db/db.go`
-	dbPath, _ := filepath.Abs(filepath.Join(cFile, "../../test_data/mock.db"))
-	db, err := gorm.Open("sqlite3", dbPath)
-	if err != nil {
-		log.Printf("when opening mock database: %v\n", err)
 	}
 	return db
 }
