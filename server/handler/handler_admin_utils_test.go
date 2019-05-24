b91413e115221758e9b31b7040db6896eef5e0f1
sentriz <senan@senan.xyz>
2019-05-24 15:23:50 +0100

    add first exiting test

---
+++ server/handler/handler_admin_utils_test.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"testing"
+)
+
+func TestFirstExisting(t *testing.T) {
+	cases := []struct {
+		name   string
+		values []string
+		or     string
+		exp    string
+	}{
+		{"none present",
+			[]string{"one", "two", "three"}, "default",
+			"one"},
+		{"first missing",
+			[]string{"", "two", "three"}, "default",
+			"two"},
+		{"middle missing",
+			[]string{"", "two", ""}, "default",
+			"two"},
+		{"all missing",
+			[]string{"", "", ""}, "default",
+			"default"},
+	}
+	for _, tc := range cases {
+		tc := tc // capture range variable?
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actu := firstExisting(tc.or, tc.values...)
+			if actu != tc.exp {
+				t.Errorf("expected %q, got %q", tc.exp, actu)
+			}
+		})
+	}
+}
