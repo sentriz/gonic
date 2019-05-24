a91ef24139b0e93be2c4a9f8ca0647cf6cad3c49
sentriz <senan@senan.xyz>
2019-05-24 15:23:50 +0100

    add first exiting test

---
+++ server/handler/handler_admin_validate.go
@@ -0,0 +1,27 @@
+package handler
+
+import "fmt"
+
+func validateUsername(username string) error {
+	if username == "" {
+		return fmt.Errorf("please enter the username")
+	}
+	return nil
+}
+
+func validatePasswords(pOne, pTwo string) error {
+	if pOne == "" || pTwo == "" {
+		return fmt.Errorf("please enter the password twice")
+	}
+	if !(pOne == pTwo) {
+		return fmt.Errorf("the two passwords entered were not the same")
+	}
+	return nil
+}
+
+func validateAPIKey(apiKey, secret string) error {
+	if apiKey == "" || secret == "" {
+		return fmt.Errorf("please enter both the api key and secret")
+	}
+	return nil
+}
