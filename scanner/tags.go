da21b5cae040805f87f6e079db951bb4719ec89b
sentriz <senan@senan.xyz>
2019-06-11 14:52:28 +0100

    explain intSep use

--- scanner/tags.go
+++ scanner/tags.go
@@ -38,9 +38,9 @@ func (t *tags) Title() string       { return t.firstTag("title") }
 func (t *tags) Artist() string      { return t.firstTag("artist") }
 func (t *tags) Album() string       { return t.firstTag("album") }
 func (t *tags) AlbumArtist() string { return t.firstTag("albumartist", "album artist") }
-func (t *tags) Year() int           { return intSep(t.firstTag("date", "year"), "-") }
-func (t *tags) TrackNumber() int    { return intSep(t.firstTag("tracknumber"), "/") }
-func (t *tags) DiscNumber() int     { return intSep(t.firstTag("discnumber"), "/") }
+func (t *tags) Year() int           { return intSep(t.firstTag("date", "year"), "-") } // eg. 2019-6-11
+func (t *tags) TrackNumber() int    { return intSep(t.firstTag("tracknumber"), "/") }  // eg. 5/12
+func (t *tags) DiscNumber() int     { return intSep(t.firstTag("discnumber"), "/") }   // eg. 1/2
 func (t *tags) Length() int         { return t.props.Length }
 func (t *tags) Bitrate() int        { return t.props.Bitrate }
 
