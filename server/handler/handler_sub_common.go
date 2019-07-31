0cf0fd05b30778b0218ff93d5f186897303317cd
Alex McGrath <amk@amk.ie>
2019-07-31 11:38:33 +0100

    big oop

--- server/handler/handler_sub_common.go
+++ server/handler/handler_sub_common.go
@@ -194,7 +194,7 @@ func (c *Controller) GetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *Controller) MusicFolderSettings(w http.ResponseWriter, r *http.Request) {
-	if _, ok := (r.URL.Query())["scanNow"]; !exists {
+	if _, ok := (r.URL.Query())["scanNow"]; !ok {
 		return
 	}
 	go func() {
