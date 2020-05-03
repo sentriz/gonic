9e4d924fd50ff0e7bb1471b77c7ffc104c6c1edf
sentriz <senan@senan.xyz>
2020-05-03 03:35:58 +0100

    simplify ctrl tests

--- server/ctrlsubsonic/handlers_by_folder_test.go
+++ server/ctrlsubsonic/handlers_by_folder_test.go
@@ -15,24 +15,24 @@ func TestGetIndexes(t *testing.T) {
 
 func TestGetMusicDirectory(t *testing.T) {
 	runQueryCases(t, testController.ServeGetMusicDirectory, []*queryCase{
-		{url.Values{"id": []string{"2"}}, "without_tracks", false},
-		{url.Values{"id": []string{"3"}}, "with_tracks", false},
+		{url.Values{"id": {"2"}}, "without_tracks", false},
+		{url.Values{"id": {"3"}}, "with_tracks", false},
 	})
 }
 
 func TestGetAlbumList(t *testing.T) {
 	runQueryCases(t, testController.ServeGetAlbumList, []*queryCase{
-		{url.Values{"type": []string{"alphabeticalByArtist"}}, "alpha_artist", false},
-		{url.Values{"type": []string{"alphabeticalByName"}}, "alpha_name", false},
-		{url.Values{"type": []string{"newest"}}, "newest", false},
-		{url.Values{"type": []string{"random"}, "size": []string{"15"}}, "random", true},
+		{url.Values{"type": {"alphabeticalByArtist"}}, "alpha_artist", false},
+		{url.Values{"type": {"alphabeticalByName"}}, "alpha_name", false},
+		{url.Values{"type": {"newest"}}, "newest", false},
+		{url.Values{"type": {"random"}, "size": {"15"}}, "random", true},
 	})
 }
 
 func TestSearchTwo(t *testing.T) {
 	runQueryCases(t, testController.ServeSearchTwo, []*queryCase{
-		{url.Values{"query": []string{"13"}}, "q_13", false},
-		{url.Values{"query": []string{"ani"}}, "q_ani", false},
-		{url.Values{"query": []string{"cert"}}, "q_cert", false},
+		{url.Values{"query": {"13"}}, "q_13", false},
+		{url.Values{"query": {"ani"}}, "q_ani", false},
+		{url.Values{"query": {"cert"}}, "q_cert", false},
 	})
 }
--- server/ctrlsubsonic/handlers_by_tags_test.go
+++ server/ctrlsubsonic/handlers_by_tags_test.go
@@ -13,32 +13,32 @@ func TestGetArtists(t *testing.T) {
 
 func TestGetArtist(t *testing.T) {
 	runQueryCases(t, testController.ServeGetArtist, []*queryCase{
-		{url.Values{"id": []string{"1"}}, "id_one", false},
-		{url.Values{"id": []string{"2"}}, "id_two", false},
-		{url.Values{"id": []string{"3"}}, "id_three", false},
+		{url.Values{"id": {"1"}}, "id_one", false},
+		{url.Values{"id": {"2"}}, "id_two", false},
+		{url.Values{"id": {"3"}}, "id_three", false},
 	})
 }
 
 func TestGetAlbum(t *testing.T) {
 	runQueryCases(t, testController.ServeGetAlbum, []*queryCase{
-		{url.Values{"id": []string{"2"}}, "without_cover", false},
-		{url.Values{"id": []string{"3"}}, "with_cover", false},
+		{url.Values{"id": {"2"}}, "without_cover", false},
+		{url.Values{"id": {"3"}}, "with_cover", false},
 	})
 }
 
 func TestGetAlbumListTwo(t *testing.T) {
 	runQueryCases(t, testController.ServeGetAlbumListTwo, []*queryCase{
-		{url.Values{"type": []string{"alphabeticalByArtist"}}, "alpha_artist", false},
-		{url.Values{"type": []string{"alphabeticalByName"}}, "alpha_name", false},
-		{url.Values{"type": []string{"newest"}}, "newest", false},
-		{url.Values{"type": []string{"random"}, "size": []string{"15"}}, "random", true},
+		{url.Values{"type": {"alphabeticalByArtist"}}, "alpha_artist", false},
+		{url.Values{"type": {"alphabeticalByName"}}, "alpha_name", false},
+		{url.Values{"type": {"newest"}}, "newest", false},
+		{url.Values{"type": {"random"}, "size": {"15"}}, "random", true},
 	})
 }
 
 func TestSearchThree(t *testing.T) {
 	runQueryCases(t, testController.ServeSearchThree, []*queryCase{
-		{url.Values{"query": []string{"13"}}, "q_13", false},
-		{url.Values{"query": []string{"ani"}}, "q_ani", false},
-		{url.Values{"query": []string{"cert"}}, "q_cert", false},
+		{url.Values{"query": {"13"}}, "q_13", false},
+		{url.Values{"query": {"ani"}}, "q_ani", false},
+		{url.Values{"query": {"cert"}}, "q_cert", false},
 	})
 }
