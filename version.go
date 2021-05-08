02cc50e4752a0f0bc47682abcf1edb069482f706
sentriz <senan@senan.xyz>
2021-05-08 18:05:37 +0100

    fix: trim newline from version

--- version.go
+++ version.go
@@ -3,10 +3,13 @@ package gonic
 
 import (
 	_ "embed"
+	"fmt"
+	"strings"
 )
 
 //go:embed version.txt
-var Version string
+var version string
+var Version = fmt.Sprintf("v%s", strings.TrimSuffix(version, "\n"))
 
 const Name = "gonic"
 const NameUpper = "GONIC"
