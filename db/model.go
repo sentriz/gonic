f0133fc6a87f4db1476871384b47dce624446731
brian-doherty <76168809+brian-doherty@users.noreply.github.com>
2022-04-21 14:01:15 -0500

    Update db/model.go
    Co-authored-by: Senan Kelly <senan@senan.xyz>

--- db/model.go
+++ db/model.go
@@ -371,8 +371,8 @@ func (pe *PodcastEpisode) SID() *specid.ID {
 	return &specid.ID{Type: specid.PodcastEpisode, Value: pe.ID}
 }
 
-func (pe *PodcastEpisode) PID() *specid.ID {
-	return &specid.ID{Type: specid.PodcastEpisode, Value: pe.PodcastID}
+func (pe *PodcastEpisode) PodcastSID() *specid.ID {
+	return &specid.ID{Type: specid.Podcast, Value: pe.PodcastID}
 }
 
 func (pe *PodcastEpisode) AudioFilename() string {
