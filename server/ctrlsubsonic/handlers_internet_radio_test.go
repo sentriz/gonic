663d2f9d5e0425849483f004f3b88931fbaaf6f0
Brian Doherty <brian.r.doherty@gmail.com>
2022-06-02 18:58:37 -0500

    Refactor to prepare for more test cases.

--- server/ctrlsubsonic/handlers_internet_radio_test.go
+++ server/ctrlsubsonic/handlers_internet_radio_test.go
@@ -3,37 +3,43 @@ package ctrlsubsonic
 import (
 	"testing"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"go.senan.xyz/gonic/server/ctrlsubsonic/spec"
   "encoding/json"
 )
 
-func TestInternetRadioStations(t *testing.T) {
-	var err error
-
-	t.Parallel()
-	contr := makeController(t)
-	
-	// Start with some bad creates
-
-	// No parameters
-	rr, req := makeHTTPMock(url.Values{})
-	contr.H(contr.ServeCreateInternetRadioStation).ServeHTTP(rr, req)
+func RunTestCase(t *testing.T, contr *Controller, h handlerSubsonic, q url.Values) (*httptest.ResponseRecorder) {
+	rr, req := makeHTTPMock(q)
+	contr.H(h).ServeHTTP(rr, req)
 	body := rr.Body.String()
 	if status := rr.Code; status != http.StatusOK {
 		t.Fatalf("didn't give a 200\n%s", body)
 	}
-	var response spec.SubsonicResponse
-	err = json.Unmarshal(rr.Body.Bytes(), &response)
+	return rr
+}
 
+func CheckMissingParameter(t *testing.T, data []byte) {
+	var response spec.SubsonicResponse
+	err := json.Unmarshal(data, &response)
 	if (err != nil) {
 		t.Fatal("json parsing failed")
 	}
 	if (response.Response.Status != "failed") {
 		t.Fatal("didn't return failed status")
 	}
-
 	if (response.Response.Error.Code != 10) {
 		t.Fatal("returned wrong error code")
 	}
+}
+
+func TestInternetRadioStations(t *testing.T) {
+	t.Parallel()
+	contr := makeController(t)
+	
+	// Start with some bad creates
+
+	// No parameters
+	rr := RunTestCase(t, contr, contr.ServeCreateInternetRadioStation, url.Values{})
+	CheckMissingParameter(t, rr.Body.Bytes())
 }
