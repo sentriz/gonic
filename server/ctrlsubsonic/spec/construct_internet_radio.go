3647f5cfa3f3f19c7c8f43d6c476e7909e5a6e03
Brian Doherty <brian.r.doherty@gmail.com>
2022-06-03 10:20:51 -0500

    Fixed some more tabs.

--- server/ctrlsubsonic/spec/construct_internet_radio.go
+++ server/ctrlsubsonic/spec/construct_internet_radio.go
@@ -4,9 +4,9 @@ import "go.senan.xyz/gonic/db"
 
 func NewInternetRadioStation(p *db.InternetRadioStation) *InternetRadioStation {
 	ret := &InternetRadioStation{
-		ID:               p.SID(),
-		Name:							p.Name,
-		StreamURL:				p.StreamURL,
+		ID:				p.SID(),
+		Name:				p.Name,
+		StreamURL:			p.StreamURL,
 		HomepageURL:			p.HomepageURL,
 	}
 	return ret
