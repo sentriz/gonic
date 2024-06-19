68b31d1632785088df7862b0484c01e01e353d56
Daniele Pintore <pintdany@gmail.com>
2024-06-19 10:29:03 +0200

    Add songLyrics extension since the getLyricsBySongId method is implemented

--- server/ctrlsubsonic/handlers_common.go
+++ server/ctrlsubsonic/handlers_common.go
@@ -41,6 +41,7 @@ func (c *Controller) ServeGetOpenSubsonicExtensions(_ *http.Request) *spec.Respo
 	sub.OpenSubsonicExtensions = &spec.OpenSubsonicExtensions{
 		{Name: "transcodeOffset", Versions: []int{1}},
 		{Name: "formPost", Versions: []int{1}},
+		{Name: "songLyrics", Versions: []int{1}},
 	}
 	return sub
 }
